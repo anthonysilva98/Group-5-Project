b3b9e8ef2032237a72c49e1d8ea03ebb996467a9
nauticgreen <eramos20@csustan.edu>
2021-05-18 10:00:48 -0700

    ?

--- app/src/main/java/com/example/userfriendlycalculator/MainActivity.java
+++ app/src/main/java/com/example/userfriendlycalculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import org.mariuszgromada.math.mxparser.*;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
@@ -140,5 +141,9 @@ public class MainActivity extends AppCompatActivity {
         String result = String.valueOf(exp.calculate());
         output.setText(result);
         output.setSelection(result.length());
+    }
+
+    public void historybutton(View view) {
+        startActivity(new Intent(MainActivity.this, history.class));
     }
 }
